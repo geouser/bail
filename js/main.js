40bd1a6368224157faf3da02452846f35029a71e
Serhiy <serhiyhulyi93@gmail.com>
2016-09-01 19:59:13 +0300

    gallery page

--- js/main.js
+++ js/main.js
@@ -138,6 +138,36 @@ jQuery(document).ready(function($) {
 
 
 
+    /*---------------------------
+                                  GALLERY
+    ---------------------------*/
+    $('.gallery__item').each( function(){
+        $(this).magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Загрузка изображения #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">Изображение #%curr%</a> не загружено.',
+                titleSrc: function(item) {
+                    return item.el.attr('title') + '<small>Парк БаиловЪ</small>';
+                }
+            }
+        });
+    });
+
+    $('.gallery__item').on('click', function(event) {
+        event.preventDefault();
+        $(this).find('a').first().click();
+    });
+
+
+
     /*----------------------------
                               SEND FORM
     -------------------------*/
